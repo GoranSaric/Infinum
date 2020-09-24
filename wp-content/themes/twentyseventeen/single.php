ebd05268a45821fd5a95c42212ba3b98364f324a
Goran <goraansaric@gmail.com>
2020-09-24 14:40:52 +0200

    single page
    banner update

--- wp-content/themes/twentyseventeen/single.php
+++ wp-content/themes/twentyseventeen/single.php
@@ -13,57 +13,14 @@
 get_header(); ?>
 
 <div class="single-post-banner">
-	<div>
-		<h3>neki tajtl</h3>
-		<p>neki autor lol</p>
+	<div class="single-intro-info">
+		<h3>The Legend of the Unicorn - Myths and Legends</h3>
+		<h5><img src="<?php bloginfo('template_url'); ?>/assets/icons/ic-writer.svg">Krešo the Duck</h5>
 	</div>
-	<a href="">back to blog</a>
-</div>
-<div>
-	<p>počnimo sa nekim paragrafom</p>
-	<span>heaven-on-earth</span>
-	<p>drugi neki paragraf</p>
-	<h4>tu neki citat sa strane</h4>
-</div>
-<div>
-	<video>tu neki videjo</video>
-	<h5>neki mali podnaslob</h5>
-</div>
-<div>
-	<h5>opet naslović</h5>
-	<p>opet neka hrpa teksta</p>
-	<h3>i opet neki citat između</h3>
-</div>
-<div>
-	<h5>opet tajtl</h5>
-	<p><span>The creature</span> kept on typing</p>
-</div>
-<div>
-	<img src="<?php bloginfo('template_url'); ?>/assets/images/img-unicorn7.png">
-	<h5>Truffle is wild mushroom</h5>
-</div>
-<div>
-	<h5>The purest creature</h5>
-	<p>opet hrpa teksta ololo</p>
-</div>
-<div>
-	<h5>wtf se događa</h5>
-	<p>sa svim ovim tekstom lol</p>
-
-</div>
-<h4>tagovi ovdje</h4>
-
-<a href="">more magic</a>
-
-<div>
-	<div>
-		<img src="">
-	</div>
-	<div>
-		<h5>mali naslov</h5>
-		<p>neki tekst za ponuđeni post</p>
-		<a href="">read more</a>
+	<div class="back-to-btn">
+	<a href="">Back to blog</a>
 	</div>
 </div>
+
 <?php
 get_footer();
